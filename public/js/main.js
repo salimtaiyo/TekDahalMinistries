2187c40fe9e2a1121e7ebe3316308abf0f2a9de0
salimtaiyo <ichigeki.ccCsc@hotmail.com>
2018-06-17 23:13:49 -0400

    added page 3 and images

--- public/js/main.js
+++ public/js/main.js
@@ -3,4 +3,14 @@ $(() => {
         $('.menu__button').toggleClass('active');
         $('.menu__list').toggle( "slow" );
     })
-})
+
+    $('.fadein p:gt(0)').hide();
+    setInterval(function()
+    {$('.fadein > :first-child').fadeOut()
+    .next('p').fadeIn().end().appendTo('.fadein');}, 3000);
+});
+
+// on scroll 
+document.querySelector('.header__title--btn')
+.addEventListener('click', () =>
+ window.scroll({ top: 625, left: 0, behavior: 'smooth' }))
